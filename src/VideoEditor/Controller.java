fc7c96eaf395b956091d7fcb7abed91b07219d88
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-02-01 23:27:56 +0100

    idk

--- src/VideoEditor/Controller.java
+++ src/VideoEditor/Controller.java
@@ -1,18 +1,30 @@
 package VideoEditor;
 
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class Controller {
-    private Stage stage;
 
-    public void setStage(Stage stage) {
+    private enum WindowState {
+        MAXIMIZED, MINIMIZED, REGULAR
+    }
+
+    private Stage stage;
+    private WindowState windowState = WindowState.REGULAR;
+    private Rectangle2D prevSize;
+
+    void setStage(Stage stage) {
         this.stage = stage;
     }
 
@@ -20,9 +32,9 @@ public class Controller {
         File startDirectory = new File(System.getProperty("user.home"));
         File[] files = startDirectory.listFiles();
         assert files != null;
-        Optional<File> videoDir= Arrays.stream(files).filter(f->f.toString().toLowerCase().contains("video")).findFirst();
-        if(videoDir.isPresent())
-            startDirectory=videoDir.get();
+        Optional<File> videoDir = Arrays.stream(files).filter(f -> f.toString().toLowerCase().contains("video")).findFirst();
+        if (videoDir.isPresent())
+            startDirectory = videoDir.get();
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select video...");
--- src/VideoEditor/Main.java
+++ src/VideoEditor/Main.java
@@ -5,19 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = loader.load();
-        Controller controller = loader.getController();
-        controller.setStage(primaryStage);
 
         primaryStage.setTitle("Video Editor");
         primaryStage.setScene(new Scene(root, 1600, 900));
+        Controller controller = loader.getController();
+        controller.setStage(primaryStage);
         primaryStage.getIcons().add(new Image("file:resource/logo.png"));
         primaryStage.show();
     }
