ec9877690dddd324eac1a8b5b7ccfafd5edf09a3
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-02-02 16:36:39 +0100

    thumbnails added

--- src/VideoEditor/Controller.java
+++ src/VideoEditor/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -56,11 +57,11 @@ public class Controller {
     private Image getThumbnail(File video) {
         String path = video.getAbsolutePath();
         String output = "thumbnails/" + getPrettyName(video) + ".png";
-        runCommand("lib/ffmpeg/windows/ffmpeg -i \"" + path + "\" -ss 00:00:01.000 -vframes 1 \"" + output + "\" -y");
+        runCommand("lib/ffmpeg/windows/ffmpeg -i \"" + path + "\" -ss 00:00:01.000 -vframes 1 \"" + output + "\" -y", false);
         return new Image("file:" + output);
     }
 
-    private void runCommand(String command) {
+    private void runCommand(String command, Boolean output) {
         try {
             Runtime rt = Runtime.getRuntime();
             Process proc;
@@ -74,16 +75,18 @@ public class Controller {
                     InputStreamReader(proc.getErrorStream()));
 
             // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
+            if(output){
+                System.out.println("Here is the standard output of the command:\n");
+                String s;
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                }
 
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
+                // read any errors from the attempted command
+                System.out.println("Here is the standard error of the command (if any):\n");
+                while ((s = stdError.readLine()) != null) {
+                    System.out.println(s);
+                }
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
